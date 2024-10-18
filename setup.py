1cd399b93d736c3a17631b77709955c89dcf2d53
Bernhard Kaindl <bernhardkaindl7@gmail.com>
2024-10-18 12:00:00 +0200

    Release 2024.10.11

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 
 setup(
     name='pre_commit_pytype',
-    version='2024.09.13',
-    install_requires=['pytype==2024.09.13'],
+    version='2024.10.11',
+    install_requires=['pytype==2024.10.11'],
 )
